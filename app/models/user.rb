28bc18cab77f87e71d50740075486c6f483887fa
ranty <ranty720@gmail.com>
2017-05-16 22:57:12 +0800

    patch /me & get /me

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
   def generate_authentication_token
     self.authentication_token = Devise.friendly_token
   end
+  mount_uploader :avatar, AvatarUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api, :defaults => { :format => :json } do
     namespace :v1 do
+      get "/me" => "users#show", :as => :user
+      patch "/me" => "users#update", :as => :update_user
+      post "/signup" => "auth#signup"
+      post "/login" => "auth#login"
+      post "/logout" => "auth#logout"
       get "/reservations" => "reservations#index", :as => :reservations
       get "/trains" => "trains#index", :as => :trains
       get "/trains/:train_number" => "trains#show", :as => :train
